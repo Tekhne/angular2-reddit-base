01345844bef1ac73699c46e264bdbc6b4f433fe2
Peter Havens <peter.havens@gmail.com>
2016-05-02 14:12:03 -0600

    Add addArticle() method which uses resolves from form.

--- app.ts
+++ app.ts
@@ -11,17 +11,32 @@ import { Component } from "angular2/core";
 
       <div class="field">
         <label for="title">Title:</label>
-        <input name="title" />
+        <input name="title" #newtitle />
       </div>
+
       <div>
         <label for="link">Link:</label>
-        <input name="link" />
+        <input name="link" #newlink />
       </div>
+
+      <button (click)="addArticle(newtitle, newlink)"
+              class="ui positive right floated button">
+        Submit link
+      </button>
     </form>
+
+    <div class="ui grid posts">
+      <reddit-article>
+      </reddit-article>
+    </div>
   `
 })
 class RedditApp {
   constructor() {
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): void {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
   }
 }
 
